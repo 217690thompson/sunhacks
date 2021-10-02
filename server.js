1e43f5c8621c2be01639800693811d9796171c93
Christian Thompson <christian@ssibtr.com>
2021-10-02 02:49:30 -0700

    Add testing url

--- server.js
+++ server.js
@@ -3,10 +3,16 @@ var url = require('url');
 var fs = require('fs');
 var express = require('express');
 var path = require('path');
+var passwordHash = require('password-hash');
+
 var database = require('./core/database.js');
 
 var app = express();
 var server = http.createServer(app);
+
+var salt = "jF9&Yt";
+var hashedPass = passwordHash.generate("password" + salt);
+console.log(passwordHash.verify("password" + salt, hashedPass));
 
 app.use('/static', express.static('static'));
 app.use('/html', express.static('html'));
@@ -24,6 +30,10 @@ async function main() {
     var collection = await database.test();
     app.get('/', (req, res) => {
         renderView(res, 'index', {fname: '/html/homepage.html'});
+    });
+    app.get('/testing', (req, res) => {
+        let file = req.query.page;
+        renderView(res, 'index', {fname: '/html/' + file + '.html'});
     });
 }
 
