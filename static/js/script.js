85b15620abd6c54356acfeb5f51687e468887b0e
Andrew Byers <bassbolero@gmail.com>
2021-10-02 22:05:48 -0700

    Fixed arrow animation

--- static/js/script.js
+++ static/js/script.js
@@ -18,13 +18,11 @@ function fadeInText(){
 
 function start(){
     image = document.querySelector(".arrow");
-    topLeftImage = document.querySelector(".top-left-arrow");
     button = document.querySelector(".btn");
     image.classList.add("arrow-clicked");
     image.classList.remove("arrow");
     $(button).fadeOut(500, function(){
         button.remove();
-        $(topLeftImage).addClass("anim");
         window.location.hash = "level=1";
     });
 }
@@ -36,7 +34,7 @@ function newLine(){
     oldP.setAttribute('id', 'oldText');
     newP.setAttribute('id', 'textOut');
     newP.style.top = (oldP.getBoundingClientRect().bottom + 4).toString() + "px";
-    arrow = document.querySelector(".top-left-arrow-new");
+    let arrow = document.querySelector(".top-left-arrow-new");
     currentArrowTop = arrow.getBoundingClientRect().top;
     // newArrowTop = currentArrowTop + 60;
     newArrowTop = newP.style.top;
@@ -74,12 +72,15 @@ function createInput(paramName) {
 }
 
 async function level1Start() {
+    let leftArrow = document.querySelector("img");
+    console.log(leftArrow);
     input = "Hello there!"
     let prom = delay(speed).then((res) => {fadeInText()});
     await prom;
+    $(leftArrow).addClass("anim");
     prom = delay(speed).then((res) => {newLine()});
     await prom;
-    input = "Welcome to this program.";
+    input = "Welcome to TypeRose.";
     fadeInText();
     prom = delay(speed).then((res) => {newLine()});
     await prom;
